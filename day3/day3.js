359f7ea05dd27868b5a038e259f76b602814e1be
Selvanayaki98 <selvi9897inbox@gmail.com>
2024-01-21 23:56:40 +0530

    day3 task pushed

--- day3/day3.js
+++ day3/day3.js
@@ -100,6 +100,7 @@ let IIFESum = 0;
 console.log(IIFESum);
 
 // d.Return all the prime numbers in an array
+
 //1, anonymous
 let numbers = [
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
